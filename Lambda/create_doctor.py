5ef05a60423528073ad3d4dff700193894b5ea79
shwetha-97 <42267522+shwetha-97@users.noreply.github.com>
2023-04-16 16:32:55 -0400

    Adding getDoctor, createDoctor and createPatient lambdas

---
+++ Lambda/create_doctor.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import datetime
+from botocore.exceptions import ClientError
+
+
+def lambda_handler(event, context):
+    print(event)
+    insert_data(event)
+
+
+def insert_data(data, db=None, table='doctors'):
+    if not db:
+        db = boto3.resource('dynamodb')
+    table = db.Table(table)
+    # overwrite if the same index is provided
+    record = transform_data(data)
+    response = table.put_item(Item=record)
+    print('@insert_data: response', response)
+    return response
+
+
+def transform_data(data):
+    record = {}
+    for key in data.keys():
+        if key == 'user_id':
+            record['doctor_id'] = data[key]
+        else:
+            record[key] = data[key]
+    return record

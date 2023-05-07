22b5d800584d3b6696be35567abc85b80984133c
shwetha-97 <42267522+shwetha-97@users.noreply.github.com>
2023-05-07 17:36:27 -0400

    Add CORS headers

--- frontend/assets/js/chat.js
+++ frontend/assets/js/chat.js
@@ -34,7 +34,12 @@ $(document).ready(function() {
           text: message
         }
       }]
-    }, {});
+    }, {headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+        }});
   }
 
 
